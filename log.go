7157ee8392b95beb5bef81269d3dc20375d1033d
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-04-30 16:04:54 +0300

    feature: move log to util

--- log.go
+++ log.go
@@ -1,5 +1,22 @@
 package welog
 
+import (
+	"fmt"
+	"time"
+)
+
+func (l *Logger) log(level LogLevel, message string) {
+	if l.level >= level {
+		timestamp := time.Now().Format(timestampFormat)
+		levelString := levelToString(level)
+		colorFunc := colorType(level)
+
+		logMessage := fmt.Sprintf("[%s] [%s] %s\n", timestamp, levelString, message)
+
+		fmt.Fprint(l.writer, colorFunc(logMessage))
+	}
+}
+
 func (l *Logger) Error(message string) {
 	l.log(Error, message)
 }
