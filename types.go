196180c7ebe98042e1042f79227a8c8050347eb3
Ando RAMANAMIHANTA <iamando@tuta.io>
2023-05-01 14:47:42 +0300

    feature: support for creating log file for errors

--- types.go
+++ types.go
@@ -12,12 +12,12 @@ type LogMode string
 type LogWrite bool
 
 type Logger struct {
-	level      LogLevel
-	mode       LogMode
-	writer     io.Writer
-	fileWriter *os.File
-	writeFile  LogWrite
-	formatter  func(LogLevel, string) string
+	level     LogLevel
+	mode      LogMode
+	writer    io.Writer
+	writeFile LogWrite
+	logFile   *os.File
+	formatter func(LogLevel, string) string
 }
 
 type ColorFormatter struct{}
